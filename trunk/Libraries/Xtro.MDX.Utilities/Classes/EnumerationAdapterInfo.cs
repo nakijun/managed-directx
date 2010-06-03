039b6fc2ffa59360137c4c98eb1c5f83ea4f3f5b
OnurER <OnurER@b12c798c-e907-11de-a21f-59b1e5349453>
2010-06-03 16:25:54 +0000

    * Managed DirectX project was suspended.

--- trunk/Libraries/Xtro.MDX.Utilities/Classes/EnumerationAdapterInfo.cs
+++ trunk/Libraries/Xtro.MDX.Utilities/Classes/EnumerationAdapterInfo.cs
@@ -3,15 +3,26 @@ using Xtro.MDX.DXGI;
 
 namespace Xtro.MDX.Utilities
 {
-    class TEnumerationAdapterInfo
+    class EnumerationAdapterInfo
     {
         public uint AdapterOrdinal;
         public AdapterDescription AdapterDescription;
         public string UniqueDescription;
         public Adapter Adapter;
 
-        public List<TEnumerationOutputInfo> OutputInfoList;
-        public List<TEnumerationDeviceInfo> DeviceInfoList;
-        public List<SEnumerationDeviceSettingsCombo> DeviceSettingsComboList;
+        public List<EnumerationOutputInfo> OutputInfoList = new List<EnumerationOutputInfo>();
+        public List<EnumerationDeviceInfo> DeviceInfoList = new List<EnumerationDeviceInfo>();
+        public List<EnumerationDeviceSettingsCombo> DeviceSettingsComboList = new List<EnumerationDeviceSettingsCombo>();
+
+        public void Delete()
+        {
+            foreach (var OutputInfo in OutputInfoList) OutputInfo.Delete();
+            OutputInfoList = null;
+            DeviceInfoList = null;
+            DeviceSettingsComboList = null;
+
+            if (Adapter != null) Adapter.Release();
+            Adapter = null;
+        }
     }
 }
--- trunk/Libraries/Xtro.MDX.Utilities/Classes/EnumerationOutputInfo.cs
+++ trunk/Libraries/Xtro.MDX.Utilities/Classes/EnumerationOutputInfo.cs
@@ -3,17 +3,19 @@ using Xtro.MDX.DXGI;
 
 namespace Xtro.MDX.Utilities
 {
-    class TEnumerationOutputInfo
+    class EnumerationOutputInfo
     {
         public uint AdapterOrdinal;
         public uint OutputOrdinal;
         public Output Output;
         public OutputDescription Description;
 
-        public List<ModeDescription> DisplayModeList;
+        public List<ModeDescription> DisplayModeList = new List<ModeDescription>();
 
         public void Delete()
         {
+            DisplayModeList = null;
+
             if (Output != null)
             {
                 Output.Release();
