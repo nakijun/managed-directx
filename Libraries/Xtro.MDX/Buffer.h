b827d8ee848fa477820f3ef007e63218871c4614
stonegoldrock@gmail.com <stonegoldrock@gmail.com@b12c798c-e907-11de-a21f-59b1e5349453>
2011-02-27 07:21:08 +0000

    #Hotfix

--- Libraries/Xtro.MDX/Buffer.h
+++ Libraries/Xtro.MDX/Buffer.h
@@ -3,10 +3,8 @@ public ref class Buffer : Resource
 private:
 	static Guid IID = IID_Converter::ToManaged(IID_ID3D10Buffer);
 
-
 internal:
 	ID3D10Buffer* pBuffer;
-
 
 	Buffer(IntPtr Buffer) : Resource(Buffer)
 	{	
